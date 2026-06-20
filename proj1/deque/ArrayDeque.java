d48c33563c592cc981c388254721b4193cf2b50c
shi-shui <1053590347@qq.com>
2026-06-20 11:21:19 +0800

    done with proj1

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,152 @@
+package deque;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static java.lang.System.arraycopy;
+
+public class ArrayDeque<T> implements Iterable<T> {
+    private int scope;
+    private int size;
+    private int head;
+    private int tail;
+    T[] ontology;
+
+    public ArrayDeque() {
+        ontology = (T[]) new Object[8];
+        scope = 8;
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    private class DequeIterator implements Iterator<T> {
+        private int wizpos;
+
+        public DequeIterator() {
+            wizpos = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return wizpos < size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            int realIndex = (head + wizpos) % scope;
+            T the_item = ontology[realIndex];
+            wizpos++;
+            return the_item;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new DequeIterator();
+    }
+
+    public void ontologyCopy(int newScope) {
+        T[] new_ontology = (T[]) new Object[newScope];
+        if (head < tail) {
+            arraycopy(ontology, head, new_ontology, 0, tail - head);
+        } else {
+            arraycopy(ontology, head, new_ontology, 0, scope - head);
+            arraycopy(ontology, 0, new_ontology, scope - head, tail);
+        }
+        head = 0;
+        tail = size;
+        scope = newScope;
+        ontology = new_ontology;
+    }
+
+    public void expansion() {
+        if (size == scope) {
+            int newScope = scope * 2;
+            ontologyCopy(newScope);
+        }
+    }
+
+    public void cutback() {
+        if (scope >= 16 && size * 4 <= scope) {
+            int newScope = Math.max(8, scope / 2);
+            ontologyCopy(newScope);
+        }
+
+    }
+
+    public void addFirst(T item) {
+        expansion();
+        size++;
+        head = (head - 1 + scope) % scope;
+        ontology[head] = item;
+    }
+
+    public void addLast(T item) {
+        expansion();
+        size++;
+        ontology[tail] = item;
+        tail = (tail + 1 + scope) % scope;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for (T the_item : this) {
+            System.out.print(the_item + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        T x = ontology[head];
+        head = (head + 1 + scope) % scope;
+        cutback();
+        return x;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        tail = (tail - 1 + scope) % scope;
+        cutback();
+        return ontology[tail];
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        int realIndex = (head + index) % scope;
+        return ontology[realIndex];
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArrayDeque<?>)) return false;
+
+        ArrayDeque<?> other = (ArrayDeque<?>) o;
+        if (this.size != other.size()) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (!this.get(i).equals(other.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,179 @@
+package deque;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedListDeque<T> implements Iterable<T> {
+
+    public static class AllNode<T> {
+        private AllNode<T> prev;
+        private final T item;
+        private AllNode<T> next;
+
+        public AllNode(AllNode<T> p, T i, AllNode<T> n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private final AllNode<T> sentinel;
+    private int size;
+
+    private class DequeIterator implements Iterator<T> {
+        private AllNode<T> current;
+
+        public DequeIterator() {
+            current = sentinel.next;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != sentinel;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            T res = current.item;
+            current = current.next;
+            return res;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new DequeIterator();
+    }
+
+    public LinkedListDeque() {
+        sentinel = new AllNode<T>(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(T x) {
+        sentinel = new AllNode<>(null, null, null);
+        sentinel.next = new AllNode<>(sentinel, x, sentinel);
+        sentinel.prev = sentinel.next;
+        size = 1;
+    }
+
+    public void addFirst(T item) {
+        size++;
+        AllNode<T> p = sentinel.next;
+        sentinel.next = new AllNode<>(sentinel, item, p);
+        p.prev = sentinel.next;
+    }
+
+    public void addLast(T item) {
+        size++;
+        AllNode<T> p = sentinel.prev;
+        sentinel.prev = new AllNode<>(p, item, sentinel);
+        p.next = sentinel.prev;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for (T the_item : this) {
+            System.out.print(the_item + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        AllNode<T> p = sentinel.next;
+        T x = p.item;
+        sentinel.next = p.next;
+        p.next.prev = sentinel;
+        return x;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        AllNode<T> p = sentinel.prev;
+        T y = p.item;
+        sentinel.prev = p.prev;
+        p.prev.next = sentinel;
+        return y;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        AllNode<T> p;
+        if (index <= size / 2) {
+            p = sentinel.next;
+            for (int i = 0; i < index; i++) {
+                p = p.next;
+            }
+        } else {
+            p = sentinel.prev;
+            int reverseIdx = size - 1 - index;
+            for (int i = 0; i < reverseIdx; i++) {
+                p = p.prev;
+            }
+        }
+        return p.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return recurseHelper(sentinel.next, index);
+    }
+
+    private T recurseHelper(AllNode<T> cur, int idx) {
+        if (idx == 0) {
+            return cur.item;
+        }
+        return recurseHelper(cur.next, idx - 1);
+    }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LinkedListDeque<?>)) return false;
+
+        LinkedListDeque<?> other = (LinkedListDeque<?>) o;
+        if (this.size != other.size) return false;
+
+        AllNode<?> curSelf = this.sentinel.next;
+        AllNode<?> curOther = other.sentinel.next;
+
+        for (int i = 0; i < size; i++) {
+            Object a = curSelf.item;
+            Object b = curOther.item;
+
+            if (a == null && b == null) {
+                curSelf = curSelf.next;
+                curOther = curOther.next;
+                continue;
+            }
+            if (a == null || b == null) return false;
+            if (!a.equals(b)) return false;
+
+            curSelf = curSelf.next;
+            curOther = curOther.next;
+        }
+        return true;
+    }
+
+}
